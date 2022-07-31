3319f8d7d586d8677eabd478bb1aecedee350ed6
ramosrodrigo <ramosrodrigo23@outlook.com>
2022-07-31 12:40:43 -0300

    comentarios sobre o polimorfismo no codigo

--- java-codigo/bytebank-heranca/src/ControleDeBonificacao.java
+++ java-codigo/bytebank-heranca/src/ControleDeBonificacao.java
@@ -2,6 +2,15 @@ public class ControleDeBonificacao {
 
     private double soma;
 
+    // referencias de tipos de classes mais genericas referenciam objetos mais especificos:
+    // Funcionario f = new Gerente();
+
+    // sobre polimorfismo:
+    // objetos ñ mudam de tipo,
+    // a referenci pode mudar, ai entra o polimorfismo,
+    // o polimorfismo permite usar referencias maos genericas para a comunicação coom o objeto,
+    // o uso de referencias mais genericas permite desacoplar sistemas.
+
     public void registra(Funcionario f){
         double boni = f.getBonificacao();
         this.soma = this.soma + boni;
